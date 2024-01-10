e5f22f6eb20965b794581403842b458f6e6e25b9
a3510377 <a102009102009@gmail.com>
2024-01-10 22:17:54 +0800

    Update MKBtn.h

--- src/MKBtn.h
+++ src/MKBtn.h
@@ -1,3 +1,27 @@
 #include <Arduino.h>
 
 #pragma once
+
+class MKBtn {
+ public:
+  MKBtn() : MKBtn(0) {}
+
+  MKBtn(uint8_t pin) : MKBtn(pin, INPUT_PULLUP) {}
+
+  MKBtn(uint8_t pin, uint8_t mode) : _pin(pin) {
+    _port = digitalPinToPort(pin);
+    _in = portInputRegister(_port);
+    _bitMask = digitalPinToBitMask(pin);
+
+    pinMode(_port, mode);
+  }
+
+ private:
+  uint8_t _pin, _port, _bitMask;
+  volatile uint8_t *_in;
+  void (*_callback)(void);
+
+  inline uint8_t _state() const {
+    return *_in & _bitMask ? HIGH : LOW;
+  }
+};
